616c0afff735989cf11d6b12a37b44a32a6bd8e3
hlong19it3 <hlong.19it3@vku.udn.vn>
2022-11-22 16:06:47 +0700

    Fix server

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
+require('dotenv').config();
+const {
+  RtcTokenBuilder,
+  RtmTokenBuilder,
+  RtcRole,
+  RtmRole,
+} = require('agora-access-token');
 
-require("dotenv").config()
-const {RtcTokenBuilder, RtmTokenBuilder, RtcRole, RtmRole} = require('agora-access-token')
+const express = require('express');
+const cors = require('cors');
 
-const express = require("express")
-const cors = require("cors");
+const app = express();
+const port = 8080;
 
-const app = express()
-const port = 8080
+app.use(cors());
 
-app.use(cors())
-
-const appId = 'be62961e14ee432ea0edc38b988b6ee3';
-const appCertificate = 'b4657d2926204606921c41982b632826';
-const channelName = 'abc';
-const uid = 1;
-const role = RtcRole.PUBLISHER;
-const expirationTimeInSeconds = 3600
-const currentTimestamp = Math.floor(Date.now() / 1000)
-const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
+const appId = process.env.APP_ID;
+const appCertificate = process.env.APP_CERTIFICATE;
+// const channelName = 'abc';
+// const uid = 1;
+// const role = RtcRole.PUBLISHER;
+const expirationTimeInSeconds = 3600;
+const currentTimestamp = Math.floor(Date.now() / 1000);
+const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
 // Build token with uid
 
-express.json()
-express.urlencoded({extended: true})
+express.json();
+express.urlencoded({ extended: true });
 
-// const generationToken = ()=> {
-//     const tokenA = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs);
-// }
+app.get('/rtc/:channelName/:role/:uid/?expiry=expireTime', async (req, res) => {
+  const { channelName, role, uid } = req.params || req.body;
+  const tokenA = await RtcTokenBuilder.buildTokenWithUid(
+    appId,
+    appCertificate,
+    channelName,
+    uid,
+    role,
+    privilegeExpiredTs
+  );
+  console.log(tokenA);
+  return res.status(200).json({ rtcToken: tokenA });
+});
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-//   })
-app.get("/token", async(req, res) => {
-     const tokenA = await RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs);
-       return res.status(200).json({rtcToken: tokenA })
-    console.log(tokenA)
-    
-})
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-
-
-// console.log("Token with integer number Uid: " + tokenA);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
