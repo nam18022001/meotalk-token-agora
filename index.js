ac69e68fc6f48fef42e77b6ae905f9021263b347
nam18022001 <hnvnam.19it3@vku.udn.vn>
2022-11-22 18:31:38 +0700

    update server

--- index.js
+++ index.js
@@ -16,20 +16,11 @@ app.use(cors());
 
 const appId = process.env.APP_ID;
 const appCertificate = process.env.APP_CERTIFICATE;
-// const channelName = 'abc';
-// const uid = 1;
-// const role = RtcRole.PUBLISHER;
-// const expirationTimeInSeconds = 3600;
-// const currentTimestamp = Math.floor(Date.now() / 1000);
-// const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
-// Build token with uid
+
 
 express.json();
 express.urlencoded({ extended: true });
 
-const expirationTimeInSeconds = 3600;
-const currentTimestamp = Math.floor(Date.now() / 1000);
-const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
 
 app.get('/rtc/:channelName/:role/:uid/?expiry=expireTime', async (req, res) => {
   const { channelName, role, uid, expireTime } = req.params || req.body;
@@ -42,7 +33,7 @@ app.get('/rtc/:channelName/:role/:uid/?expiry=expireTime', async (req, res) => {
     channelName,
     uid,
     role,
-    privilegeExpiredTs
+    expireTime
   );
   // console.log(tokenA);
   return res.status(200).json({ rtcToken: tokenA });
