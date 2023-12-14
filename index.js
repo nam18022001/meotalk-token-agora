590ddad8d3c6a19b3b4269909a6fd7029cc4e86b
justbeenam <hoangngvietnam@gmail.com>
2023-12-14 15:27:50 +0700

    commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
+const md5 = require("md5");
 const {
   RtcTokenBuilder,
   RtmTokenBuilder,
   RtcRole,
   RtmRole,
-} = require('agora-access-token');
+} = require("agora-access-token");
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = 3001;
@@ -17,29 +18,26 @@ app.use(cors());
 const appId = process.env.APP_ID;
 const appCertificate = process.env.APP_CERTIFICATE;
 
-
 express.json();
 express.urlencoded({ extended: true });
 
-
-
-app.get('/rtc/:channelName/:role/uid/:uid', async (req, res) => {
+app.get("/rtc/:channelName/:role/uid/:uid", async (req, res) => {
   const { channelName, role, uid } = req.params || req.body;
   const expirationTimeInSeconds = 3600;
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
-  // console.log(privilegeExpiredTs);
+  const channel = md5(channelName);
 
   const tokenA = await RtcTokenBuilder.buildTokenWithUid(
     appId,
     appCertificate,
-    channelName,
+    channel,
     uid,
     role,
     privilegeExpiredTs
   );
   // console.log(tokenA);
-  return res.status(200).json({ rtcToken: tokenA });
+  return res.status(200).json({ rtcToken: tokenA, channelCall: channel });
 });
 
 app.listen(port, () => {
